1e2e61764e125cf5bd06a3b2e4931202f45fbe7c
Geek_Tech <geecktech.och2@gmail.com>
2024-10-09 20:45:21 +0600

    task1_1

---
+++ jss/script.js
@@ -0,0 +1,8 @@
+let startButton = document.querySelector('#start');
+let game = document.querySelector('#game');
+let timeHeader = document.querySelector('#time-header');
+let resultHeader = document.querySelector('#result-header');
+let timeDisplay = document.querySelector('#time');
+let resultDisplay = document.querySelector('#result');
+let gameTimeInput = document.querySelector('#game-time');
+
